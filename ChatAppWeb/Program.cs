89d2ff44d7fa8276163bad88c1c12a9fa3d1d231
MortenStaunstrup <mort490fgromling@gmail.com>
2026-03-29 23:09:01 +0200

    Changed actions file to cp appsettings.Production.json to properties folder for Production deployment, and now uses that json file instead of hardcoded localhost, Azure socket variables

--- ChatAppWeb/Program.cs
+++ ChatAppWeb/Program.cs
@@ -2,16 +2,24 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ChatAppWeb;
+using DotNetEnv;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+string? baseUrl = builder.Configuration["BaseApiPath"];
+
+if (baseUrl is null)
+{
+    throw new ApplicationException("BaseUrl is not set");
+}
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredLocalStorageAsSingleton();
 builder.Services.AddScoped(sp => new HttpClient
 {
-    //BaseAddress = new Uri("https://chatappmeapi.azurewebsites.net/api/")
-    BaseAddress = new Uri("http://localhost:5154/api/")
+    BaseAddress = new Uri(baseUrl)
 });
 
 await builder.Build().RunAsync();

322bb42828e5a9a32d43560dfba4f1c15341583b
MortenStaunstrup <mort490fgromling@gmail.com>
2026-03-30 17:29:59 +0200

    Added use of appsettings.json, reworked the CI/CD pipeline file

--- ChatAppWeb/Program.cs
+++ ChatAppWeb/Program.cs
@@ -2,24 +2,21 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ChatAppWeb;
-using DotNetEnv;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-string? baseUrl = builder.Configuration["BaseApiPath"];
-
-if (baseUrl is null)
-{
-    throw new ApplicationException("BaseUrl is not set");
-}
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredLocalStorageAsSingleton();
-builder.Services.AddScoped(sp => new HttpClient
+builder.Services.AddScoped(sp =>
 {
-    BaseAddress = new Uri(baseUrl)
+    var config = sp.GetRequiredService<IConfiguration>();
+    var baseUrl = config["BaseApiPath"];
+    if (string.IsNullOrEmpty(baseUrl))
+        throw new ApplicationException("BaseUrl is not set");
+    return new HttpClient { BaseAddress = new Uri(baseUrl) };
+    
 });
 
 await builder.Build().RunAsync();

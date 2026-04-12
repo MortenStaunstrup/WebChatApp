b4f7c68b63eb1763733e4d705dcbfba0bba662e9
MortenStaunstrup <mort490fgromling@gmail.com>
2026-04-12 21:38:02 +0200

    Forgot to add changed userrepositorymongodb refactor to use more dependency injection

--- ChatAppAPI/Program.cs
+++ ChatAppAPI/Program.cs
@@ -5,6 +5,7 @@ using ChatAppAPI.Token;
 using DotNetEnv;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +17,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 
-builder.Services.AddSingleton<IUserRepository, UserRepositoryMongoDb>();
+
 builder.Services.AddSingleton<IMessagesRepository, MessageRepositoryMongoDb>();
 builder.Services.AddSingleton<IConversationRepository, ConversationRepositoryMongoDb>();
 builder.Services.AddSingleton<TokenProvider>();
@@ -35,6 +36,29 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ClockSkew = TimeSpan.Zero,
         };
     });
+
+builder.Services.AddSingleton<IMongoClient>(sp =>
+{
+    var connectionString = Environment.GetEnvironmentVariable("MONGO_CONNECTION_STRING");
+
+    if (string.IsNullOrWhiteSpace(connectionString))
+        throw new InvalidOperationException("MONGO_CONNECTION_STRING environment variable is not set");
+    return new MongoClient(connectionString);
+});
+
+builder.Services.AddScoped<IMongoDatabase>(sp =>
+{
+    var mongoClient = sp.GetRequiredService<IMongoClient>();
+    
+    var databaseName = Environment.GetEnvironmentVariable("MONGO_DATABASE_NAME");
+    
+    if (string.IsNullOrWhiteSpace(databaseName))
+        throw new InvalidOperationException("MONGO_DATABASE_NAME environment variable is not set");
+    
+    return mongoClient.GetDatabase(databaseName);
+});
+
+builder.Services.AddScoped<IUserRepository, UserRepositoryMongoDb>();
 
 builder.Services.AddCors(options =>
 {
